27d5ef140fd3172a796b3907b8166b4a9e6b9599
Blesssings0 <antoniojul789gmail.com>
2024-07-29 05:13:59 -0400

    Lista navbar del header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,23 @@
 export default function Header() {
   return (
     <div>
-      <h1 className="text-center text-4xl font-bold">Header</h1>
+      <h1 className="text-center text-4xl font-bold">Hello word!</h1>
+      <nav>
+        <li>
+          <ul>
+            <a href="">services</a>
+          </ul>
+          <ul>
+            <a href="">services</a>
+          </ul>
+          <ul>
+            <a href="">services</a>
+          </ul>
+          <ul>
+            <a href="">services</a>
+          </ul>
+        </li>
+      </nav>
     </div>
   );
 }
