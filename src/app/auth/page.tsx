a373f5f8b7bf584e834603d30585fc2bfa20dd29
Yuzu <Yuzu_0204@outlook.com>
2024-07-30 00:07:25 -0400

    feat : Add Loader Common Component and complete the Auth Page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,10 +1,27 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-//?* Future implementation import { Redirect } from "next";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Loader from "@/components/common/Loader";
 
 export default function Auth() {
-  const { data: session } = useSession();
-  return session ? redirect("/") : redirect("/login");
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "loading") return; // Espera a que se cargue la sesión
+
+    if (session) {
+      router.push("/"); // Redirige si hay sesión
+    } else {
+      router.push("/login"); // Redirige a login si no hay sesión
+    }
+  }, [session, status, router]);
+
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Loader />
+    </div>
+  );
 }
---
+++ src/components/common/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader: React.FC = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="loader h-16 w-16 animate-spin rounded-full border-4 border-solid border-blue-500 border-t-transparent"></div>
+    </div>
+  );
+};
+
+export default Loader;
