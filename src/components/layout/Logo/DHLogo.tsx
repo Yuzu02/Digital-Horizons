48fe91edfe8f5f8f3a2828971afcc75df090ad63
Blesssings0 <antoniojul789gmail.com>
2024-08-07 23:55:43 -0400

    fix: :bug: Solucionando otro error en el header y el logo
    Solucionando otro error en el header y el logo
    
    Solucionando otro error en el header y el logo

--- src/components/layout/Logo/DHLogo.tsx
+++ src/components/layout/Logo/DHLogo.tsx
@@ -1,5 +1,9 @@
-function DHLogo() {
-  return <div> El Logo va aqui</div>;
-}
+import Image from "next/image";
 
-export default DHLogo;
+export default function DHLogo() {
+  return (
+    <div>
+      <Image className="Logos" src="" alt="Digital Horizons" />
+    </div>
+  );
+}
