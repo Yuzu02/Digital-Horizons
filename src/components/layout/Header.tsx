dcc492b7f9b3b00137a48d2c29255c2be5bffde0
Yuzu <Yuzu_0204@outlook.com>
2024-08-08 00:23:19 -0400

    fix: Fix broken Header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,27 +1,25 @@
 // Todo : Hacer el header de la pagina
 
-import { IoLogoInstagram } from "react-icons/io5";
+import Link from "next/link";
 // ? import DHLogo from "@/components/layout/Logo/DHLogo";
 
 export default function Header() {
   return (
     <div>
-      <div className="size-[50px] items-center">
-        <IoLogoInstagram />
-      </div>
+      <div className="size-[50px] items-center"></div>
       <nav>
         <li>
           <ul>
-            <a href="">services</a>
+            <Link href="">services</Link>
           </ul>
           <ul>
-            <a href="">services</a>
+            <Link href="">services</Link>
           </ul>
           <ul>
-            <a href="">services</a>
+            <Link href="">services</Link>
           </ul>
           <ul>
-            <a href="">services</a>
+            <Link href="">services</Link>
           </ul>
         </li>
       </nav>
