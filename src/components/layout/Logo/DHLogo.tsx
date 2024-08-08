bcfac742b9884b4446d8d5860dbdc3e190fc2826
Blesssings0 <antoniojul789gmail.com>
2024-08-08 18:46:59 -0400

    my message

--- src/components/layout/Logo/DHLogo.tsx
+++ src/components/layout/Logo/DHLogo.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function DHLogo() {
   return (
     <div>
-      <Image className="Logos" src="" alt="Digital Horizons" />
+      <Image
+        className="Logos"
+        src="/DHlogo/Screenshot 2024-07-29 225640.png"
+        alt="Digital Horizons"
+      />
     </div>
   );
 }
