442a529fa73121cfae4bdae1ba9d4d3e1ce10002
Leticia Encarnacion Lopez <leticiaencarnacionlopez2002@gmail.com>
2024-08-15 01:55:09 -0400

    categorias...

--- src/utils/data/categories/constants.ts
+++ src/utils/data/categories/constants.ts
@@ -1,6 +1,5 @@
 import {
   FaRobot,
-  FaBrain,
   FaChartPie,
   FaBitcoin,
   FaBullhorn,
@@ -14,11 +13,17 @@ import { SiVirtualbox } from "react-icons/si";
 
 export const categories = [
   {
-    name: "Aprendizaje-Automático",
-    icon: FaBrain,
+    name: "Ciencia de Datos",
+    icon: FaChartPie,
     description:
-      "Optimiza procesos y deja que la tecnología haga el trabajo por ti.",
-    href: "/blog/categories/Aprendizaje-Automático",
+      "Analiza grandes volúmenes de datos para tomar decisiones inteligentes.",
+    href: "/blog/categories/Ciencia-de-Datos",
+  },
+  {
+    name: "Desarrollo Web y Aplicaciones",
+    icon: FaLaptopCode,
+    description: "Crea y desarrolla con las mejores prácticas de programación.",
+    href: "/blog/categories/Desarrollo-Web&Aplicaciones",
   },
   {
     name: "IA",
@@ -28,55 +33,42 @@ export const categories = [
     href: "/blog/categories/ia",
   },
   {
-    name: "Robótica&Automatización",
+    name: "Internet de las Cosas",
+    icon: FaFire,
+    description: "Mantente al día con lo más caliente y novedoso del momento.",
+    href: "/blog/categories/Internet-de-las-Cosas",
+  },
+  {
+    name: "Robótica y Automatización",
     icon: MdMotionPhotosAuto,
     description:
       "La robótica y automatización han revolucionando la industria y la vida cotidiana.",
     href: "/blog/categories/Robótica&Automatización",
   },
   {
-    name: "Ciencia-de-Datos",
-    icon: FaChartPie,
-    description:
-      "Analiza grandes volúmenes de datos para tomar decisiones inteligentes.",
-    href: "/blog/categories/Ciencia-de-Datos",
+    name: "Redes Sociales y Marketing",
+    icon: FaBullhorn,
+    description: "Conquista el mercado online con estrategias innovadoras.",
+    href: "/blog/categories/mRedes-Sociales&Marketing",
   },
   {
-    name: "Tecnología-Financiera",
+    name: "Tecnología Financiera",
     icon: FaBitcoin,
     description:
       "Explora el mundo de las monedas digitales y sus oportunidades.",
     href: "/blog/categories/Tecnología-Financiera",
   },
   {
-    name: "Redes-Sociales&Marketing",
-    icon: FaBullhorn,
-    description: "Conquista el mercado online con estrategias innovadoras.",
-    href: "/blog/categories/mRedes-Sociales&Marketing",
-  },
-  {
-    name: "Tecnología-e-Innovación",
-    icon: FaStar,
-    description: "Opiniones y análisis detallados de los últimos productos.",
-    href: "/blog/categories/Tecnología-e-Innovación",
-  },
-  {
-    name: "Desarrollo-Web&Aplicaciones",
-    icon: FaLaptopCode,
-    description: "Crea y desarrolla con las mejores prácticas de programación.",
-    href: "/blog/categories/Desarrollo-Web&Aplicaciones",
-  },
-  {
-    name: "Internet-de-las-Cosas",
-    icon: FaFire,
-    description: "Mantente al día con lo más caliente y novedoso del momento.",
-    href: "/blog/categories/Internet-de-las-Cosas",
-  },
-  {
-    name: "Realidad-Virtual&Mundos-Digitales",
+    name: "Realidad Virtual y Mundos Digitales",
     icon: SiVirtualbox,
     description:
       "crea entornos de simulaciones digitales, transformando la manera en que jugamos y aprendemos.",
     href: "/blog/categories/Realidad-Virtual&Mundos-Digitales",
+  },
+  {
+    name: "Tecnología e Innovación",
+    icon: FaStar,
+    description: "Opiniones y análisis detallados de los últimos productos.",
+    href: "/blog/categories/Tecnología-e-Innovación",
   },
 ];
